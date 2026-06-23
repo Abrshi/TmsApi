c8199c92722c3cbe7f1091f5ec97bce4e0f98583
Abreham Yeshitla <abrhamyeshitla990@gmail.com>
2026-06-24 02:24:37 +0300

    test

--- TsmApi/obj/Debug/net10.0/TsmApi.AssemblyInfo.cs
+++ TsmApi/obj/Debug/net10.0/TsmApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TsmApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b75e09e0d1bbe23f2f62a7ee258eb451737c8b8f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+925cc19d45ad4ab69fb3255932a713eb57fc9226")]
 [assembly: System.Reflection.AssemblyProductAttribute("TsmApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TsmApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
