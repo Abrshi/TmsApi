0d8e671c0769501dcf38af69b2395b49febfe4dc
Abreham Yeshitla <abrhamyeshitla990@gmail.com>
2026-06-26 11:00:18 +0300

    fix : Part B fixing N+1 performance eshu

--- TsmApi/Exercises/Exercise7.cs
+++ TsmApi/Exercises/Exercise7.cs
@@ -1,23 +1,20 @@
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using TmsApi.Data;
 
 namespace TmsApi.Exercises;
+
 public static class Exercise7
 {
     public static async Task Run(TmsDbContext db)
     {
         var students = await db.Students
             .AsNoTracking()
+            .Include(s => s.Enrollments)
             .ToListAsync();
 
         foreach (var s in students)
         {
-            var count = await db.Enrollments
-                .AsNoTracking()
-                .CountAsync(e => e.StudentId == s.Id);
-
-            Console.WriteLine($"{s.Name}: {count} enrollments");
+            Console.WriteLine($"{s.Name}: {s.Enrollments.Count} enrollments");
         }
     }
 }
