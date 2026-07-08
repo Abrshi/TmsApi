b8ee7c24a05545d190508ef351b7f1b7667a562e
Abreham Yeshitla <abrhamyeshitla990@gmail.com>
2026-07-08 11:35:06 +0300

    fix : make the ms sutable for m6

--- TsmApi/Configurations/CourseConfiguration.cs
+++ TsmApi/Configurations/CourseConfiguration.cs
@@ -10,12 +10,12 @@ public class CourseConfiguration : IEntityTypeConfiguration<Course>
     {
         builder.HasKey(c => c.Id);
 
-        builder.Property(c => c.Title)
-            .IsRequired()
-            .HasMaxLength(150);
+        builder.Property(c => c.Code).IsRequired().HasMaxLength(10);
 
-        builder.HasMany(c => c.Enrollments)
-            .WithOne(e => e.Course)
-            .HasForeignKey(e => e.CourseId);
+        builder.Property(c => c.Title).IsRequired().HasMaxLength(150);
+
+        builder.HasIndex(c => c.Code).IsUnique();
+
+        builder.HasMany(c => c.Enrollments).WithOne(e => e.Course).HasForeignKey(e => e.CourseId);
     }
 }
