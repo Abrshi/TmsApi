b05916f075b72f58f10e110857489a7f41e88cde
Abreham Yeshitla <abrhamyeshitla990@gmail.com>
2026-07-13 07:43:14 +0300

    fet: dto

---
+++ TsmApi/Controllers/CoursesController.cs
@@ -0,0 +1,40 @@
+using Microsoft.AspNetCore.Mvc;
+using Tms.Api.Dtos;
+using TmsApi.Services;
+
+namespace TmsApi.Controllers;
+
+[ApiController]
+[Route("api/courses")]
+public class CoursesController(ICourseService courseService)
+    : ControllerBase
+{
+    [HttpGet("{id:int}", Name = nameof(GetCourseById))]
+    public async Task<IActionResult> GetCourseById(
+        int id,
+        CancellationToken ct)
+    {
+        var course = await courseService.GetByIdAsync(id, ct);
+
+        if (course is null)
+        {
+            return NotFound();
+        }
+
+        return Ok(course);
+    }
+
+
+    [HttpPost]
+    public async Task<IActionResult> CreateCourse(
+        CreateCourseRequest request,
+        CancellationToken ct)
+    {
+        var result = await courseService.CreateAsync(request, ct);
+
+        return CreatedAtAction(
+            nameof(GetCourseById),
+            new { id = result.Id },
+            result);
+    }
+}
