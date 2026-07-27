36fae1fd1ee3a115145828bf89fbba13277801dc
abrshi <abrhamyeshitla990@gmail.com>
2026-07-27 01:44:50 -0700

    m7s1 stap 6

---
+++ TmsApi.Application/Interfaces/ICourseRepository.cs
@@ -0,0 +1,10 @@
+using TmsApi.Domain.Entities;
+
+namespace TmsApi.Application.Interfaces;
+
+public interface ICourseRepository
+{
+    Task<Course?> GetByCodeAsync(
+        string courseCode,
+        CancellationToken ct = default);
+}
